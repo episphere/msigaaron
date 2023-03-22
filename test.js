19efdd9464430f2f5f5795b64a7d8b0536996a01
Aaron Ge <aaronge13@icloud.com>
2023-03-22 15:09:51 -0400

    Made all the heatmaps double clustered

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
 mSigSDK = await (await import("../main.js")).mSigSDK;
-getMutationalSignatureEtiologyOptions = mSigSDK.mSigPortal.mSigPortalData.getMutationalSignatureEtiologyOptions()
-
-cancerType = "Lung-AdenoCA";
-mutationalSignatures = await mSigSDK.mSigPortal.mSigPortalData.getMutationalSignaturesData("WGS", "COSMIC_v3_Signatures_GRCh37_SBS96", "SBS", 10000)
-mutationalSignaturesExtracted = await mSigSDK.mSigPortal.mSigPortalData.extractMutationalSpectra(mutationalSignatures, "signatureName")
-
-umapVisualizationData = await mSigSDK.mSigPortal.mSigPortalData.getMutationalSpectrumData(
+visualizationData = await mSigSDK.mSigPortal.mSigPortalData.getMutationalSpectrumData(
     "PCAWG",
     null,
     "WGS",
@@ -13,8 +7,6 @@ umapVisualizationData = await mSigSDK.mSigPortal.mSigPortalData.getMutationalSpe
     "SBS",
     96,
 );
+extractedData = mSigSDK.mSigPortal.mSigPortalData.extractMutationalSpectra(visualizationData)
 
-patientMutationalSpectra = await mSigSDK.mSigPortal.mSigPortalData.extractMutationalSpectra(umapVisualizationData, "sample")
-nnlsExposures = await mSigSDK.fitMutationalSpectraToSignatures(mutationalSignaturesExtracted, patientMutationalSpectra)
-
-mSigSDK.plotMutationalSignatureExposure(nnlsExposures["SP50263"], "exposureGraph", "SP50263")
+cosSimilarity = mSigSDK.mSigPortal.mSigPortalPlots.plotCosineSimilarityHeatMap(extractedData, "PCAWG", "WGS", "Lung-AdenoCA", "cosineSimilarityHeatMap");
